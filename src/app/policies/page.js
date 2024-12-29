8a7fb18d6080d7b7ec42259b7095a56e7e11072a
JeasonCB <camposjeason@gmail.com>
2024-12-28 20:02:12 -0400

    second commit

---
+++ src/app/policies/page.js
@@ -0,0 +1,16 @@
+function page() {
+    return (
+        <div>
+            <h1>Ejemplo de Políticas de la Empresa</h1>
+            <p>Bienvenido a la página de políticas de nuestra empresa. Aquí encontrará ejemplos de políticas y procedimientos importantes.</p>
+            <h2>Política de Privacidad</h2>
+            <p>Nos tomamos muy en serio la privacidad de nuestros usuarios. Aquí puede encontrar detalles sobre cómo manejamos su información personal.</p>
+            <h2>Política de Uso Aceptable</h2>
+            <p>Esta política describe las reglas y normas que debe seguir al utilizar nuestros servicios.</p>
+            <h2>Política de Cookies</h2>
+            <p>Utilizamos cookies para mejorar su experiencia en nuestro sitio web. Aquí puede encontrar más información sobre cómo utilizamos las cookies.</p>
+        </div>
+    );
+}
+
+export default page;
---
+++ src/app/terms/page.jsx
@@ -0,0 +1,16 @@
+function TermsPage() {
+    return (
+        <div>
+            <h1>Términos del Servicio</h1>
+            <p>Estos términos regulan el uso de nuestros servicios. Al acceder o utilizar nuestro sitio, usted acepta cumplir con lo siguiente:</p>
+            <h2>Uso de la Plataforma</h2>
+            <p>Solo puede utilizar la plataforma con fines lícitos y respetando todas las leyes y regulaciones aplicables.</p>
+            <h2>Responsabilidades del Usuario</h2>
+            <p>Usted es responsable de cualquier actividad que ocurra en su cuenta, así como de la veracidad de la información proporcionada.</p>
+            <h2>Limitaciones de Responsabilidad</h2>
+            <p>No nos hacemos responsables de daños o pérdidas causados por incumplimientos de estos términos o por acciones de terceros.</p>
+        </div>
+    );
+}
+
+export default TermsPage;
